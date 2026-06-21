1d8457fc6caaf2ae765344cf17df47fa561fed41
Akshitha Daroju <akshithadaroju@gmail.com>
2026-06-21 13:16:17 +0000

    Replace PyMuPDF4LLM with PyMuPDF

--- llm.py
+++ llm.py
@@ -1,16 +1,21 @@
-from llama_cpp import Llama
+from groq import Groq
+import streamlit as st
 
-llm = Llama(
-    model_path="models/tinyllama-1.1b-chat-v1.0-q4_k_m.gguf",
-    n_ctx=2048,
-    verbose=False
+client = Groq(
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
 def ask_llm(prompt):
-    response = llm(
-        prompt,
-        max_tokens=200,
-        temperature=0.2
+    response = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.2,
+        max_tokens=512
     )
 
-    return response["choices"][0]["text"]
+    return response.choices[0].message.content
